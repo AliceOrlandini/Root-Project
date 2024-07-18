47963ac04ba8fba36394681d0cf221170af9897f
Alice Orlandini <62644820+AliceOrlandini@users.noreply.github.com>
2024-07-18 22:52:35 +0200

    create histogram

--- docker-root/root-python/main.py
+++ docker-root/root-python/main.py
@@ -14,6 +14,9 @@ def plot():
     mytree = f.Get("ParticlesEvents")
     zpos_lkr = 241093. # distance from the target to the calorimeter
 
+    h = ROOT.TH1F("h1", "MyHistogram", 100, 57838.40254981886, 138633.60501040035)  
+    vertex_min = 1000000000
+    vertex_max = -1000000000
     # loop over the entries
     for event in mytree: 
         # get the neutral and charged particles
@@ -27,6 +30,11 @@ def plot():
                 # calculate the vertex
                 vertex= zpos_lkr - cp[0].fVertexKTAG.z()
 
+                if (vertex < vertex_min):
+                    vertex_min = vertex
+                if (vertex > vertex_max):
+                    vertex_max = vertex
+
                 xn0 = np[0].fPosition.x()
                 xn1 = np[1].fPosition.x()
                 yn0 = np[0].fPosition.y()
@@ -36,8 +44,17 @@ def plot():
 
                 vertex_n = math.sqrt(((xn0 - xn1)**2 + (yn0 - yn1)**2) * en0 * en1) / 135.
                 if (abs(vertex - vertex_n) < 4000.):
-                    print(f"vertex: {vertex}; vertex_n: {vertex_n}; vertex-vertex_n: {vertex-vertex_n}")
+                    #print(f"vertex: {vertex}; vertex_n: {vertex_n}; vertex-vertex_n: {vertex-vertex_n}")
 
+                    # crea un istogramma il vertice calcolato
+                    h.Fill(vertex)
+
+    print(f"vertex_min: {vertex_min}; vertex_max: {vertex_max}")
+    c1 = ROOT.TCanvas()
+    h.Draw() 
+    c1.SaveAs("histogram.png")
+
+    #ROOT.gApplication.Run()
     f.Close()
 
 if __name__ == "__main__":
